38379cdc75489aa292557d03b1baaa76e6c9c277
Mirko Bobanović <mibobano@vsite.hr>
2017-03-22 20:47:02 +0100

    lab4

--- PotapanjeBrodova/Test/TestMreže.cs
+++ PotapanjeBrodova/Test/TestMreže.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+
 
 namespace Test
 {
@@ -7,9 +10,81 @@ namespace Test
     public class TestMreže
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Mreža_DajSlobodnaPoljaVraćaSvaPoljaZaPočetnuMrežu()
         {
-            int a = 3;
+            Mreža m = new Mreža(5, 5);
+            Assert.AreEqual(25, m.DajSlobodnaPolja().Count());
         }
+
+        [TestMethod]
+        public void Mreža_DajSlobodnaPoljaVraćaS24PoljaZaMrežu5x5NakonJednogUklonjenogPoljaZadanogRetkomIStupcem()
+        {
+            Mreža m = new Mreža(5, 5);
+            m.UkloniPolje(1, 1);
+            Assert.AreEqual(24, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+        }
+
+        [TestMethod]
+        public void Mreža_DajSlobodnaPoljaVraćaS24PoljaZaMrežu5x5NakonJednogUklonjenogPolja()
+        {
+            Mreža m = new Mreža(5, 5);
+            m.UkloniPolje(new Polje(1, 1));
+            Assert.AreEqual(24, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+        }
+        [TestMethod]
+        public void Mreža_DajSlobodnaPoljaVraćaS23PoljaZaMrežu5x5NakonDvaUklonjenogPolja()
+        {
+            Mreža m = new Mreža(5, 5);
+            m.UkloniPolje(1, 1);
+            m.UkloniPolje(4, 4);
+            Assert.AreEqual(23, m.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+            Assert.IsFalse(m.DajSlobodnaPolja().Contains(new Polje(4, 4)));
+        }
+
+        [TestMethod]
+       
+        public void Mreža_UkloniPoljeBacaIznimkuZaNepostojećiRedak()
+        {
+            try
+            {
+                Mreža m = new Mreža(5, 5);
+                m.UkloniPolje(6, 1);
+                Assert.Fail();
+            }
+            catch(IndexOutOfRangeException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch(Exception)
+            {
+                Assert.Fail();
+            }
+           
+        }
+
+        [TestMethod]
+
+        public void Mreža_UkloniPoljeBacaIznimkuZaNepostojećiStupac()
+        {
+            try
+            {
+                Mreža m = new Mreža(5, 5);
+                m.UkloniPolje(1, 6);
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+        }
+
     }
 }
