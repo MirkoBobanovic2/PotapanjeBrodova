cf0409f3bee5ae2f896c5d88930786562f1319bd
Mirko Bobanović <mibobano@vsite.hr>
2017-04-26 20:29:36 +0200

    promjenaa

---
+++ PotapanjeBrodova/PotapanjeBrodova/IGađani.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public enum RezultatGađanja
+    {
+        Promašaj,
+        Pogodak,
+        Potopljen
+    }
+    interface IGađani
+    {
+        RezultatGađanja Gađaj(Polje polje);
+    }
+}
