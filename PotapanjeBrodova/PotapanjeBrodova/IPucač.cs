b6c9dcc02372f9f985886185e232b76b8038e20d
Mirko Bobanović <mibobano@vsite.hr>
2017-05-03 20:46:17 +0200

    Topništvo

---
+++ PotapanjeBrodova/PotapanjeBrodova/IPucač.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public interface IPucač
+    {
+        Polje Gađaj();
+        void ObradiGađanje(RezultatGađanja rezultat);
+
+       
+    }
+}
